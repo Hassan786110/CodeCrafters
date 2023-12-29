02446834b0e45f194edd97e0c57b9a166e727339
Muhammad Hassan <92513804+Hassan786110@users.noreply.github.com>
2023-12-30 01:46:47 +0500

    practice2

---
+++ practice2/app.js
@@ -0,0 +1,149 @@
+// alert("This is an alert");
+// window.alert("this is an alert");
+
+// Data Types
+// 1.String
+// 2.Number
+// 3.Boolean
+
+// intialization
+// declaration
+// var name = "Muhammad Hassan";
+
+// intialize
+// var name;
+
+// declaration
+// name = "Muhammad Hassan";
+
+// console.log(name);
+
+// var weight = 150;
+// weight = weight + 25;
+// weight += 25;
+// console.log(weight);
+
+// var num = 9 + "9";
+// var num = 9 + "9";
+
+// this typeof is used to check the datatype of variable
+// typeof num
+
+// ways to convert string into number
+// 1.Number()
+// 2.parseInt()
+// 3.+
+
+// var num = Number("9");
+// var num = parseInt("9");
+// var num = +"9";
+
+// var num = 9 + +"9";
+// console.log(num);
+
+// illegal variable names
+// var 1name = "Muhammad"
+// var nam&e = "Muhammad";
+// var n-a+me = "Muhammad"
+
+// Modulus %
+// var num = 5;
+// console.log(5 % 3);
+
+// var num = 9;
+// Increment
+
+// Pre Increment
+// ++num;
+
+// Post Increment
+// num++;
+
+// Decrement
+
+// Pre Decrement
+// ++num;
+
+// Post Decrement
+// num++;
+
+// console.log(num);
+
+// BODMAS
+
+// Concatination
+// var first = "Muhammad";
+// var last = "Hassan";
+// alert(first + " " + last);
+
+// var name = prompt("Enter your Name", "E.g John Duckett");
+// console.log(name);
+
+// alert("Muhammad\n\nHassan");
+// window.prompt
+
+// if (false) {
+//   console.log("Muhammad Hassan");
+// }
+
+// javascript run top to bottom and left to right
+// javascript is a case sensitive language
+
+// == compare values
+// === compare values and their datatypes
+
+// Operators
+// && AND
+// || OR
+// ! NOT
+
+// var input = prompt("Enter your name");
+// var first = "Muhammad";
+
+// if (first !== input) {
+//   console.log("Worked");
+// } else if () {
+
+// } else {
+//   console.log("Not Worked");
+// }
+
+// Array
+// collection of multiple value
+// set of multiple value
+
+// var fruit = ["apple", "banana", "grapes"];
+
+// index# unique id of every value
+// var fruitName = "apple";
+
+// console.log(fruit[1]);
+
+// Array Methods
+// pop (deletes last value)
+// push (add values in the end of the array)
+// shift (delete first value)
+// unshift (add values in the start of the array)
+// splice (add, delete at any index)
+
+// Array and String Method
+// slice (make the copy)
+
+// console.log(fruit);
+// fruit.pop();
+// console.log(fruit);
+
+// fruit.push("pomogranate", "tty");
+// console.log(fruit);
+
+// for loop Example:
+// for(var i = 0; i < 5; i++) {}
+
+// for loop: It takes 3 expressions
+// Expression 1 sets a variable before the loop starts (var i = 0).
+// Expression 2 defines the condition for the loop to run (i must be less than 5).
+// Expression 3 increases a value (i++) each time the code block in the loop has been executed.
+
+// for (var i = 0; i < 100; i++) {
+//   console.log(i);
+// }
